88021f00998d9fdadec2a5b3248952ea70b700d4
Mattis GHEZAL <mattis.ghezal@bordeaux-inp.fr>
2026-03-09 11:34:47 +0100

    Front v1

---
+++ prototype-front/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/logo.png' }
+      ]
+    }
+  }
+})
